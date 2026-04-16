2bfe2432319e4718eb53adbde08c65caa15fa5a7
BAHATI Robben <bahatiroben@gmail.com>
2026-04-16 13:16:15 +0200

    implementing the base controler into auth module

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,23 +3,25 @@ import { LoginDto } from './dtos/login.dto';
 import { AuthService } from './auth.service';
 import { IAuthResult } from './interfaces';
 import { CreateUserDto } from '../users/createUser.dto.ts/createUser.dto';
+import { BaseController } from 'src/common/base';
+import { ApiResponse } from 'src/common/dto';
 
 @Controller('auth')
-export class AuthController {
+export class AuthController extends BaseController {
 
     constructor(private authService: AuthService) {
-        
+        super();
     }
 
     @Post('login')
     login(
         @Body() loginDto: LoginDto
-    ): Promise<IAuthResult> {
-        return this.authService.authenticate(loginDto);
+    ): Promise<ApiResponse<IAuthResult>> {
+        return this.createResponse(() => this.authService.authenticate(loginDto), 'Login successful');
     }
 
     @Post('register')
-    register(@Body() createUserDto: CreateUserDto): Promise<IAuthResult> {
-        return this.authService.register(createUserDto)
+    register(@Body() createUserDto: CreateUserDto): Promise<ApiResponse<IAuthResult>> {
+        return this.createResponse(() => this.authService.register(createUserDto), 'User registered successfully');
     }
 }
