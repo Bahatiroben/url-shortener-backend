2e50d864c48ce691cadfd1d057c386279d969b48
BAHATI Robben <bahatiroben@gmail.com>
2026-04-16 12:25:30 +0200

    adding the filters and interceptors to main app file and adding the api/v1 global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters';
+import { ResponseInterceptor } from './common/interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    // Global Response Wrapper
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
+  // Global Exception Filter
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Global validation pipe
   app.useGlobalPipes(new ValidationPipe())
+
+  // Other configurations (CORS, ValidationPipe, Swagger, etc.)
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+  
   await app.listen(3000);
 }
 bootstrap();
